904370f1a9d1ccd1d22789c39659f8ae13aaf1f2
안용성 <pj.yamdeng2@kalmate.net>
2024-07-16 17:13:52 +0900

    formview 제네레이트

--- generate-string.js
+++ generate-string.js
@@ -108,8 +108,58 @@ const <%= fileName %> = create<any>((set, get) => ({
 
 export default <%= fileName %>`;
 
+const formViewGenerateString = `
+import withSourceView from '@/hooks/withSourceView';
+import { useEffect } from 'react';
+/* store 경로를 변경해주세요. */
+import <%= storeName %> from '@/stores/guide/<%= storeName %>';
+
+function <%= fileName %>() {
+
+  /* formStore state input 변수 */
+  const { <% tableColumns.forEach((columnInfo)=> { %> <%= columnInfo.column_name %>,<% }) %> errors, changeInput, save, clear } =
+    <%= storeName %>();
+
+  useEffect(() => {
+    return clear();
+  }, []);
+
+  return (
+    <>
+      <div className="grid-one-container">
+        <% tableColumns.forEach((columnInfo)=> { %>        
+        <div className="div-label"><%= columnInfo.column_comment %> :</div>
+        <div className="div-input">
+          <input
+            type="text"
+            className={errors.<%= columnInfo.column_name %> ? 'input-not-valid' : ''}
+            placeholder="<%= columnInfo.column_comment %>"
+            name="<%= columnInfo.column_name %>"
+            id="<%= columnInfo.column_name %>"
+            value={<%= columnInfo.column_name %>}
+            onChange={(event) => changeInput('<%= columnInfo.column_name %>', event.target.value)}
+          />
+          {errors.<%= columnInfo.column_name %> ? <span className="error_message">{errors.<%= columnInfo.column_name %>}</span> : null}
+        </div>        
+        <% }) %>
+        <div className="right" style={{ width: 580 }}>
+          <button className="button button-cancel" onClick={clear}>
+            취소
+          </button>
+          <button className="button button-info" onClick={save}>
+            저장
+          </button>
+        </div>
+      </div>
+    </>
+  );
+}
+export default <%= fileName %>;
+`;
+
 module.exports = {
   testGenerateString: testGenerateString,
   listComponentGenerateString: listComponentGenerateString,
   formStoreGenerateString: formStoreGenerateString,
+  formViewGenerateString: formViewGenerateString,
 };
